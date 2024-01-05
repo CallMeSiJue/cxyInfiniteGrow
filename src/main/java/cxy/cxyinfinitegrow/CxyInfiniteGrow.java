fa8785eabb938b916c2d97e0ef9bf4afd6baa56a
池 <1105553966@qq.com>
2024-01-05 15:39:08 +0800

    first commit

---
+++ src/main/java/cxy/cxyinfinitegrow/CxyInfiniteGrow.java
@@ -0,0 +1,10 @@
+package cxy.cxyinfinitegrow;
+
+import net.fabricmc.api.ModInitializer;
+
+public class CxyInfiniteGrow implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/cxy/cxyinfinitegrow/client/CxyInfiniteGrowClient.java
@@ -0,0 +1,10 @@
+package cxy.cxyinfinitegrow.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CxyInfiniteGrowClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
